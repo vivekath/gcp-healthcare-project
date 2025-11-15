b2dd6b349df3e6e99a7ca5173137eb90a376c554
vivek <vivek.athilkar100@gmail.com>
2025-11-15 15:59:37 +0530

    Update corrections.py

--- data/INGESTION/WORKING_CODE/corrections.py
+++ data/INGESTION/WORKING_CODE/corrections.py
@@ -25,4 +25,5 @@ gcloud dataproc clusters create ${CLUSTER_NAME} \
 # Setting SQL instance => 4. Setting up the Data sources - SQL DBs, GCS, BQ, Configs => 3:07 / 48:20
 # Setting Cloud Compose => 6. GCS Landing to Bronze Layer - GCS, BigQuery => 23:49 / 1:03:59
 # Create Dags - Workflow Orchestration - Airflow => 19:30 / 28:46
+# 
 # Setting Cloud Build trigger CICD => 10. CICD - Github, Cloud Build, Airflow => 13:50 / 1:20:26
