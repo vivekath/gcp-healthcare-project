e951a9de6623da3c6af1d8cf884abf431bc068e5
vivekathilkar-bit <vivek.athilkar100@gmail.com>
2025-12-27 20:36:03 +0530

    Revert "testing"
    This reverts commit a7fb6991cdd4c505aa496c4601172eb54b101180.

--- workflows/dataflow_dag.py
+++ workflows/dataflow_dag.py
@@ -47,7 +47,7 @@ ARGS = {
 with DAG(
     dag_id="dataflow_dag",
     default_args=ARGS,
-    schedule_interval=None,
+    schedule_interval=get_var("SCHEDULE_INTERVAL"),   # or cron
     description="This is a dataflow dag",
     catchup=False,
     tags=["dataflow", "beam"],
@@ -58,7 +58,15 @@ with DAG(
     # -------------------------------------------------------------------------
     transactions_pipeline = BeamRunPythonPipelineOperator(
         task_id="transactions_dataflow_job",
-        py_file=f"gs://{COMPOSER_BUCKET}/data/INGESTION/transactions_pipeline.py"
+        py_file=f"gs://{COMPOSER_BUCKET}/data/INGESTION/transactions_pipeline.py",
+        pipeline_options={
+            "project": PROJECT_ID,
+            "region": REGION,
+            "runner": "DataflowRunner",
+            "job_name": "transactions",
+            "temp_location": f"gs://{GCS_BUCKET}/temp/",
+            "staging_location": f"gs://{GCS_BUCKET}/staging/",
+        },
     )
 
     # -------------------------------------------------------------------------
@@ -66,7 +74,15 @@ with DAG(
     # -------------------------------------------------------------------------
     retail_sales_pipeline = BeamRunPythonPipelineOperator(
         task_id="retail_sales_dataflow_job",
-        py_file=f"gs://{COMPOSER_BUCKET}/data/INGESTION/retail_sales_pipeline.py"
+        py_file=f"gs://{COMPOSER_BUCKET}/data/INGESTION/retail_sales_pipeline.py",
+        pipeline_options={
+            "project": PROJECT_ID,
+            "region": REGION,
+            "runner": "DataflowRunner",
+            "job_name": "retail_sales",
+            "temp_location": f"gs://{GCS_BUCKET}/temp/",
+            "staging_location": f"gs://{GCS_BUCKET}/staging/",
+        },
     )
 
     # -------------------------------------------------------------------------
